57a112cf55c22205023710af3a54e82dbee5f432
Jirayu Smitthimedhin (Ben) <65589688+jsmitthimedhin@users.noreply.github.com>
2021-07-13 16:00:53 +0700

    Add files via upload

---
+++ src/main/java/com/ben/springboot/crud/dao/ChurchDAOJpaImpl.java
@@ -0,0 +1,48 @@
+package com.ben.springboot.crud.dao;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ben.springboot.crud.entity.Member;
+
+@Repository
+public class ChurchDAOJpaImpl implements ChurchDAO {
+	
+	private EntityManager entityManager;
+	
+	@Autowired
+	public ChurchDAOJpaImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public List<Member> findAll() {
+		Query query = entityManager.createQuery("FROM Member", Member.class);
+		List<Member> members = query.getResultList();
+		System.out.println("Getting the query: " + members);
+		return members;
+	}
+	
+	@Override
+	public Member getMember(int id) {
+		Member member = entityManager.find(Member.class, id);
+		return member;
+	}
+	
+	@Override
+	public void saveMember(Member newMember) {
+			entityManager.merge(newMember);
+		}
+
+	@Override
+	public void deleteMember(int id) {
+		Member member = entityManager.find(Member.class, id);
+		entityManager.remove(member);
+			
+	}
+
+}
